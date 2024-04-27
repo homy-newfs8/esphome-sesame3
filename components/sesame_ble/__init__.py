767a8e66a5f7c66f53fdb8302bcb14d2ec344c2c
homy <horigit@newfs.to>
2024-04-27 23:50:57 +0900

    sesame_ble: autoload esp32_ble_tracker

--- components/sesame_ble/__init__.py
+++ components/sesame_ble/__init__.py
@@ -3,14 +3,17 @@ import esphome.config_validation as cv
 from esphome.components import esp32_ble_tracker
 from esphome.const import CONF_ID
 
+AUTO_LOAD = ["esp32_ble_tracker"]
 DEPENDENCIES = ["esp32_ble_tracker"]
 
 sesame_ble_ns = cg.esphome_ns.namespace("sesame_ble")
 SesameBleListener = sesame_ble_ns.class_("SesameBleListener", esp32_ble_tracker.ESPBTDeviceListener)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SesameBleListener),
-}).extend(esp32_ble_tracker.ESP_BLE_DEVICE_SCHEMA)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(SesameBleListener),
+    }
+).extend(esp32_ble_tracker.ESP_BLE_DEVICE_SCHEMA)
 
 
 async def to_code(config):
