9a9c83d9422ad6045092c845f4d8c44e8ded1ac3
Naoki Sawada <1412630+nao-pon@users.noreply.github.com>
2026-08-10 14:03:06 +0900

    Add LOCKING/UNLOCKING states for lock operations (#17)
    * Add LOCKING/UNLOCKING states for lock operations

--- components/sesame/lock_feature.cpp
+++ components/sesame/lock_feature.cpp
@@ -492,12 +492,22 @@ SesameLock::control(const lock::LockCall& call) {
 	if (!operable_warn()) {
 		return;
 	}
+
 	if (call.get_state()) {
 		auto tobe = *call.get_state();
+
 		if (tobe == lock::LOCK_STATE_LOCKED) {
-			parent_->sesame.lock(default_history_tag);
+			if (parent_->sesame.lock(default_history_tag)) {
+				publish_state(lock::LOCK_STATE_LOCKING);
+			} else {
+				ESP_LOGW(TAG, "Failed to send lock command");
+			}
 		} else if (tobe == lock::LOCK_STATE_UNLOCKED) {
-			parent_->sesame.unlock(default_history_tag);
+			if (parent_->sesame.unlock(default_history_tag)) {
+				publish_state(lock::LOCK_STATE_UNLOCKING);
+			} else {
+				ESP_LOGW(TAG, "Failed to send unlock command");
+			}
 		}
 	}
 }
